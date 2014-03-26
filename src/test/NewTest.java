d2e50386eabb87c9d716b21cda824365315cd0aa
helloleonzhao <helloleon@gmail.com>
2014-03-25 22:52:36 -0700

    v1

---
+++ src/test/NewTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("TestNG3");
+  }
+  
+ // public static void main() {
+ // System.out.println("Done");
+ //  }
+}
